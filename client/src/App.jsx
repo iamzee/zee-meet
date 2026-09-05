8ccd45fcaf4ab1e555a5ed52c6dbb3c4b1004d70
Zeeshan Adeen <zeeshan.adeen@newtonschool.co>
2026-09-05 23:56:18 +0530

    feat: add create form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,9 +11,48 @@ const App = () => {
       });
   }, []);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const title = event.target.elements.title.value;
+    const description = event.target.elements.description.value;
+
+    const newEvent = {
+      title,
+      description,
+    };
+
+    fetch("http://localhost:3000/events", {
+      method: "POST",
+      body: JSON.stringify(newEvent),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents([...events, data]);
+        event.target.elements.title.value = "";
+        event.target.elements.description.value = "";
+      });
+  };
+
   return (
     <div>
       <h1>ZeeMeet</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input id="title" required name="title" placeholder="Enter title" />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          required
+          name="description"
+          placeholder="Enter description"
+        ></textarea>
+        <button>Save</button>
+      </form>
+
       {!events ? (
         <p>Loading...</p>
       ) : (
